270bf2bc4ad9a2dfba4d3d51a2778c8b59a4790d
Luis Orts <luisorts@correo.ugr.es>
2023-12-10 14:43:32 +0100

    parece que solo falta frontend

--- src/main/java/es/javaschool/train/TrainApplication.java
+++ src/main/java/es/javaschool/train/TrainApplication.java
@@ -2,6 +2,10 @@ package es.javaschool.train;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class TrainApplication  {
@@ -9,6 +13,15 @@ public class TrainApplication  {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         SpringApplication.run(TrainApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+            }
+        };
     }
 
 
